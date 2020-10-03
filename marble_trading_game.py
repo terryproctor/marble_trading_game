cd526ea06012f5ab0eb4b02338f16d7b825031b4
terryproctor <terryproctor2015@outlook.com>
2020-10-03 23:07:57 +0100

    see last comments for todo

--- marble_trading_game.py
+++ marble_trading_game.py
@@ -5,6 +5,7 @@ import random
 starting_purse = 1000
 purse = 0
 purse += starting_purse
+current_round = 0
 
 marbles = []
 marbles.extend(["red"]*4)
@@ -47,3 +48,21 @@ def check_status():
     print(f"Reds: {red}, Green: {green}")
 
 #ask user to place a bet
+def ask_user_bet(purse):
+    while True:
+        try: 
+            round_bet = int(input("How much would you like to bet"))
+        except ValueError:
+            print(f"This is not a valid input")
+            continue
+        if purse < round_bet:
+            print(f"Try again, purse: {purse}")
+        elif round_bet < 1:
+            print("Bets must be £1 or more")
+        else: 
+            return round_bet
+
+#function for draw and consequences
+
+#function to check if game over (current round == totals rounds)
+# or purse < 0.5 * starting purse
