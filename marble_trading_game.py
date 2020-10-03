544f5764a69747257a3198ac6213e46d5a4169ec
terryproctor <terryproctor2015@outlook.com>
2020-10-03 22:29:30 +0100

    initial commit

---
+++ marble_trading_game.py
@@ -0,0 +1,44 @@
+import random
+
+#A trading game based on a real-life simulation used to coach new  traders
+
+starting_purse = 1000
+
+marbles = []
+marbles.extend(["red"]*4)
+marbles.extend(["green"]*6)
+# print(marbles)
+
+red = marbles.count("red")
+green = marbles.count("green")
+
+#function to shuffle up marbles
+def shuffle_bag(marbles):
+    random.shuffle(marbles)
+    return marbles
+
+#function to ask for how many rounds to play
+def rounds_to_play():
+    while True:        
+        try:
+            total_rounds = int(input("How many round to play? (up to 9)\n")) 
+        except ValueError:
+            print("This is not a valid number")
+            continue
+        if (total_rounds > 0 and total_rounds < 10):
+            break 
+        return total_rounds
+
+#printing the rules for the user
+def print_rules():
+    print("""
+    Bet before each round.
+    Each time a coloured marble is drawn and removed from the bag.
+    If you draw a green marble, you win the amount bet.
+    If you draw a red marble you lose the amount bet.
+    The game will end when either rounds are up, or your purse is less than £500.
+    """)
+
+#checking purse and green and red left
+def check_status():
+
